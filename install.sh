8e942b3a3d88eadcdc80c26a18914af750eb4cb1
Dodge W Coates <dodge.w.coates@gmail.com>
2020-01-19 10:38:37 -0500

    Add simple install script.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+binary_path="$HOME/bin"
+declare -a scripts=(screenshot.sh screencast.sh)
+
+echo "Adding scripts files to $binary_path: ${scripts[@]}"
+for c in "${scripts[@]}"; do
+    C=$(echo "$c" | sed -rn 's/(.*)\.sh/\1/p')
+    P="$binary_path/$C"
+
+    if [ -L ${P} ]; then
+        echo "Removing old symlink: $C"
+        rm $P
+    fi
+
+    ln -s $(dirname "$0")/$c $P
+done
