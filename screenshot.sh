554b0a352f65d835f37e3e39888f41ea53c7ffc6
Dodge Coates <dodge@goldbox.localdomain>
2020-01-30 14:30:30 -0500

    Fixup screenshot.

--- screenshot.sh
+++ screenshot.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-TMPFILE="$(mktemp -t screenshot-XXXXXXX).png"
-OUTPUT="$HOME/Screenshots/$(date +%F-%H-%M-%S)"
-
-notify-send "starting..."
+NAME="$(date +%F-%H-%M-%S).png"
+OUTPUT="$HOME/Screenshots/$NAME"
 
 read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i")
-shutter --select="[$X,$Y,$W,$H]"
+shutter --select="[$X,$Y,$W,$H]" --exit_after_capture --min_at_startup --output="$OUTPUT"
 
-notify-send "Screenshot taken"
-
-notify-send "size $(du -h $OUTPUT.gif | awk '{print $1}')"
-
-eog $OUTPUT.gif
-
-trap "rm -f '$TMPFILE'" 0
+notify-send --icon=$OUTPUT --urgency=low "Screenshot taken" "size: $(du -h $OUTPUT | awk '{print $1}')\nname: $NAME"
