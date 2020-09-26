f81226de7e1c7a3b9fb168f9747b98332a4bc08a
Dodge W. Coates (goldbox) <dodge@chess.com>
2020-09-26 17:32:40 -0400

    Fix caps on system resume.

--- install.sh
+++ install.sh
@@ -25,3 +25,25 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo apt install shutter
     sudo apt install xcape
 fi
+
+echo "Creating suspend/resume script for capslock behavior..."
+FIX_CAPS_COMMANDS="/usr/bin/setxkbmap -option ctrl:swapcaps && /usr/bin/xcape -e \'Control_L=Escape\'"
+
+function add_fix_caps_on_login() {
+    if ! grep -q $FIX_CAPS_COMMANDS "$1"; then
+        echo $FIX_CAPS_COMMANDS >> "$1"
+    fi
+}
+
+add_fix_caps_on_login $HOME/.bashrc
+add_fix_caps_on_login $HOME/.bash_profile
+add_fix_caps_on_login $HOME/.profile
+
+# Fix caps on system resume
+CAPS_SCRIPT="case \'\$1\' in\n    resume)\n\t\t$FIX_CAPS_COMMANDS\nesac\n"
+CAPS_SCRIPT_PATH="/usr/lib/pm-utils/sleep.d/1000fix-capslock"
+printf '#!/bin/bash\n\n' > /tmp/caps_script
+printf "$CAPS_SCRIPT" >> /tmp/caps_script
+sudo mv /tmp/caps_script $CAPS_SCRIPT_PATH
+
+echo "Done."
